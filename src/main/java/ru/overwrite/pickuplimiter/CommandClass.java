e5ff653159d265300c5dfa63f66197b0a189c52b
Overwrite987 <artem.korolev_2003@mail.ru>
2025-02-26 05:31:19 +0300

    simplify command class (not final version)

--- src/main/java/ru/overwrite/pickuplimiter/CommandClass.java
+++ src/main/java/ru/overwrite/pickuplimiter/CommandClass.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import ru.overwrite.pickuplimiter.configuration.Config;
+import ru.overwrite.pickuplimiter.configuration.data.Messages;
 import ru.overwrite.pickuplimiter.utils.MaterialUtils;
 
 import java.util.*;
@@ -29,92 +30,93 @@ public class CommandClass implements CommandExecutor, TabCompleter {
             plugin.getLogger().info("Только для игроков");
             return true;
         }
+        Messages messages = pluginConfig.getMessages();
         if (!p.hasPermission("pickuplimiter.use")) {
-            p.sendMessage(pluginConfig.getMessages().noPerm());
+            p.sendMessage(messages.noPerm());
             return true;
         }
         if (args.length < 1 || args.length > 3) {
-            p.sendMessage(pluginConfig.getMessages().usage());
+            p.sendMessage(messages.usage());
             return true;
         }
         String playerName = p.getName();
         switch (args[0].toLowerCase()) {
             case "enable": {
                 if (databaseManager.getCachedPlayers().containsKey(playerName)) {
-                    p.sendMessage(pluginConfig.getMessages().alreadyEnabled());
+                    p.sendMessage(messages.alreadyEnabled());
                     return true;
                 }
                 databaseManager.enableForPlayer(playerName);
-                p.sendMessage(pluginConfig.getMessages().enabled());
+                p.sendMessage(messages.enabled());
                 return true;
             }
             case "disable": {
                 if (databaseManager.getCachedPlayers().containsKey(playerName)) {
                     databaseManager.disableForPlayer(playerName);
-                    p.sendMessage(pluginConfig.getMessages().disabled());
+                    p.sendMessage(messages.disabled());
                     return true;
                 }
-                p.sendMessage(pluginConfig.getMessages().alreadyDisabled());
+                p.sendMessage(messages.alreadyDisabled());
                 return true;
             }
             case "add": {
                 if (args.length < 2) {
-                    p.sendMessage(pluginConfig.getMessages().usage());
+                    p.sendMessage(messages.usage());
                     return true;
                 }
                 String material = args[1].toUpperCase();
                 if (!MaterialUtils.MATERIAL_NAMES.contains(material)) {
-                    p.sendMessage(pluginConfig.getMessages().incorrectMaterial());
+                    p.sendMessage(messages.incorrectMaterial());
                     return true;
                 }
                 if (!databaseManager.getCachedPlayers().containsKey(playerName)) {
                     databaseManager.addPlayer(playerName, pluginConfig.getMainSettings().defaultEnabled(), Set.of(new String[]{material}));
-                    p.sendMessage(pluginConfig.getMessages().blockSuccess().replace("%material%", material));
+                    p.sendMessage(messages.blockSuccess().replace("%material%", material));
                     return true;
                 }
                 Set<String> newSet = new HashSet<>(databaseManager.getCachedPlayers().get(playerName));
                 if (newSet.contains(material)) {
-                    p.sendMessage(pluginConfig.getMessages().alreadyBlocked());
+                    p.sendMessage(messages.alreadyBlocked());
                     return true;
                 }
                 newSet.add(material);
                 databaseManager.updatePlayer(playerName, newSet);
-                p.sendMessage(pluginConfig.getMessages().blockSuccess().replace("%material%", material));
+                p.sendMessage(messages.blockSuccess().replace("%material%", material));
                 return true;
             }
             case "remove": {
                 if (args.length < 2) {
-                    p.sendMessage(pluginConfig.getMessages().usage());
+                    p.sendMessage(messages.usage());
                     return true;
                 }
                 String material = args[1].toUpperCase();
                 if (!MaterialUtils.MATERIAL_NAMES.contains(material)) {
-                    p.sendMessage(pluginConfig.getMessages().incorrectMaterial());
+                    p.sendMessage(messages.incorrectMaterial());
                     return true;
                 }
                 Set<String> newSet = new HashSet<>(databaseManager.getCachedPlayers().get(playerName));
                 if (!newSet.contains(material)) {
-                    p.sendMessage(pluginConfig.getMessages().notBlocked().replace("%material%", material));
+                    p.sendMessage(messages.notBlocked().replace("%material%", material));
                     return true;
                 }
                 newSet.remove(material);
                 if (newSet.isEmpty()) {
                     databaseManager.removePlayer(playerName);
-                    p.sendMessage(pluginConfig.getMessages().unblockSuccess().replace("%material%", material));
+                    p.sendMessage(messages.unblockSuccess().replace("%material%", material));
                     return true;
                 }
                 databaseManager.updatePlayer(playerName, newSet);
-                p.sendMessage(pluginConfig.getMessages().unblockSuccess().replace("%material%", material));
+                p.sendMessage(messages.unblockSuccess().replace("%material%", material));
                 return true;
             }
             case "list": {
                 Set<String> blocked = databaseManager.getCachedPlayers().get(p.getName());
                 if (blocked == null) {
-                    p.sendMessage(pluginConfig.getMessages().list().replace("%list%", "NaN"));
+                    p.sendMessage(messages.list().replace("%list%", "NaN"));
                     return true;
                 }
                 blocked = new TreeSet<>(databaseManager.getCachedPlayers().get(playerName));
-                p.sendMessage(pluginConfig.getMessages().list().replace("%list%", blocked.toString()));
+                p.sendMessage(messages.list().replace("%list%", blocked.toString()));
                 return true;
             }
         }
